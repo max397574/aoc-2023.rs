b419f34b501892d3ef0d158013fbc9064a094d9f
Max <max.heinzer397@gmx.ch>
2023-12-04 17:28:00 +0100

    feat(4/2): initial solution

--- src/solutions/day4.rs
+++ src/solutions/day4.rs
@@ -1,4 +1,5 @@
 use bstr::ByteSlice;
+// TODO: use bitmap and logical and to find matches
 
 pub fn part_1(input: &str) -> impl std::fmt::Display {
     let mut score = 0;
@@ -38,7 +39,44 @@ pub fn part_1(input: &str) -> impl std::fmt::Display {
 }
 
 pub fn part_2(input: &str) -> impl std::fmt::Display {
-    0
+    let mut matching_numbers = Vec::new();
+    for line in input.as_bytes().lines() {
+        let mut line_score = 0;
+        let mut getting_winning_numbers = true;
+        let mut winning_numbers = Vec::new();
+        for block in line.split(|&byte| byte == b' ') {
+            if block.len() == 0 {
+                continue;
+            }
+            if block[0] == b'C' {
+                continue;
+            }
+            if block.len() == 2 && block[1] == b':' {
+                continue;
+            }
+            if block[0] == b'|' {
+                getting_winning_numbers = false;
+                continue;
+            }
+            if getting_winning_numbers {
+                winning_numbers.push(block);
+            } else {
+                if winning_numbers.contains(&block) {
+                    line_score += 1;
+                }
+            }
+        }
+        matching_numbers.push(line_score);
+    }
+    let mut copies = vec![1; matching_numbers.len()];
+    for (idx, match_count) in matching_numbers.iter().enumerate() {
+        for i in 1..=*match_count {
+            copies[idx + i] += 1 * copies[idx];
+        }
+    }
+    let mut count = 0;
+    copies.iter().for_each(|x| count += x);
+    count
 }
 
 #[cfg(test)]
@@ -50,7 +88,13 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-    const INPUT2: &str = "";
+
+    const INPUT2: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     #[test]
     fn _part1() {
@@ -59,6 +103,6 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     #[test]
     fn _part2() {
-        assert_eq!(part_2(INPUT2).to_string(), String::from("0"))
+        assert_eq!(part_2(INPUT2).to_string(), String::from("30"))
     }
 }
