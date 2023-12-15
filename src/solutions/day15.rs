4ff06b08ab596234f01b2d59acffe1fb49c33780
Max <max.heinzer397@gmx.ch>
2023-12-15 10:37:58 +0100

    feat(15/1): intial solution

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -1,24 +1,47 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-    0
+use bstr::ByteSlice;
+
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    let mut sum: u32 = 0;
+    let mut tmp_sum: u32 = 0;
+    input
+        .as_bytes()
+        .lines()
+        .next()
+        .unwrap()
+        .iter()
+        .for_each(|&b| match b {
+            b',' => {
+                sum += tmp_sum;
+                tmp_sum = 0;
+            }
+            // b'\n' => (),
+            _ => {
+                tmp_sum += b as u32;
+                tmp_sum *= 17;
+                tmp_sum %= 256;
+            }
+        });
+    sum + tmp_sum
 }
 
 pub fn part_2(_input: &str) -> impl std::fmt::Display {
     0
 }
 
-// #[cfg(test)]
+#[cfg(test)]
 mod tests {
     use super::*;
-    const _INPUT1: &str = "";
-    const _INPUT2: &str = "";
+    const INPUT1: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    const INPUT2: &str = "";
 
-    // #[test]
-    fn _part1() {
-        assert_eq!(part_1(_INPUT1).to_string(), String::from("0"))
+    #[test]
+    fn part1() {
+        assert_eq!(part_1(INPUT1).to_string(), String::from("1320"))
     }
 
-    // #[test]
-    fn _part2() {
-        assert_eq!(part_2(_INPUT2).to_string(), String::from("0"))
+    #[test]
+    fn part2() {
+        assert_eq!(part_2(INPUT2).to_string(), String::from("0"))
     }
 }
+
